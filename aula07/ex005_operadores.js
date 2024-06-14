dcb7c44fd3e1927820d013a5c7d8bacbed389671
Thiago Oliveira <thiagosoaresccb@gmail.com>
2024-06-13 21:04:29 -0300

    Aula 07

---
+++ aula07/ex005_operadores.js
@@ -0,0 +1,13 @@
+console.log("Operadores aritméticos:\n")
+soma = 5 + 2
+console.log(`Soma entre 5 e 2 = ${soma}`)
+subtracao = 5 - 2
+console.log(`Substração entre 5 e 2 = ${subtracao}`)
+multiplicacao = 5 * 2
+console.log(`Multiplicação entre 5 e 2 = ${multiplicacao}`)
+divisao = 5 / 2
+console.log(`Divisão entre 5 e 2 = ${divisao}`)
+modulo = 5 % 2
+console.log(`Resto da divisão entre 5 e 2 = ${modulo}`)
+exponeciacao = 5 ** 2
+console.log(`5 elevado a 2 = ${exponeciacao}`)
