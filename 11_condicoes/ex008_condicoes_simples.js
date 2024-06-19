11cd776acab5fc8eb27a8094075d8326fbe18b25
Thiago Oliveira <thiagosoaresccb@gmail.com>
2024-06-18 22:02:28 -0300

    Aulas 11

---
+++ 11_condicoes/ex008_condicoes_simples.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade é ${vel}km/h`)
+if(vel > 60) {
+    console.log("Você ultrapassou o limite de velocidade. MULTADO!")
+  console.log("Dirija com cuidado.")
+}
+console.log("Use sempre o cinto de segurança!")
